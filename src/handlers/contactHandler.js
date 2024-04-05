2ba04c8f79725a847f44d3848f8ee99473be0712
Brajesh825 <brajeshmishra825@gmail.com>
2024-04-05 17:04:37 +0530

    contact us hero data modified

--- src/handlers/contactHandler.js
+++ src/handlers/contactHandler.js
@@ -1,5 +1,11 @@
 import { clearContent } from "./utils/utils";
 
+import HeroBeta from "../components/heroBeta";
+import footerAlpha from "../components/footer/footerAlpha";
+
+import footerData from "../datasets/footerData";
+import { contactUsHeroData } from "../datasets/contactUsData";
+
 function contactHandler() {
     // Scroll to the top
     window.scrollTo(0, 0);
